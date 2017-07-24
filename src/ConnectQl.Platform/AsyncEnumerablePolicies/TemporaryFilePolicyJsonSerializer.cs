6ec855005e145db6cd98a781cc31f8a3347fa125
Maarten van Sambeek <maarten.x@gmail.com>
2017-07-24 21:40:07 +0200

    Added platform plugin.

---
+++ src/ConnectQl.Platform/AsyncEnumerablePolicies/TemporaryFilePolicyJsonSerializer.cs
@@ -0,0 +1,140 @@
+﻿// MIT License
+//
+// Copyright (c) 2017 Maarten van Sambeek.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace ConnectQl.Platform.AsyncEnumerablePolicies
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+    using ConnectQl.AsyncEnumerablePolicies;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// The JSON row serializer.
+    /// </summary>
+    internal class TemporaryFilePolicyJsonSerializer : ISerializer
+    {
+        /// <summary>
+        /// The readers.
+        /// </summary>
+        private readonly Dictionary<Stream, JsonTextReader> readers = new Dictionary<Stream, JsonTextReader>();
+
+        /// <summary>
+        /// The serializer.
+        /// </summary>
+        private readonly JsonSerializer serializer = new JsonSerializer
+                                                         {
+                                                             TypeNameHandling = TypeNameHandling.Auto
+                                                         };
+
+        /// <summary>
+        /// The writers.
+        /// </summary>
+        private readonly Dictionary<Stream, JsonTextWriter> writers = new Dictionary<Stream, JsonTextWriter>();
+
+        /// <summary>
+        /// Reads items from the stream.
+        /// </summary>
+        /// <param name="stream">
+        /// The stream.
+        /// </param>
+        /// <param name="count">
+        /// The number of items to read.
+        /// </param>
+        /// <typeparam name="T">
+        /// The type of the items.
+        /// </typeparam>
+        /// <returns>
+        /// The items.
+        /// </returns>
+        public Task<IEnumerable<T>> ReadAsync<T>(Stream stream, long count)
+        {
+            return Task.FromResult(this.ReadAsyncInternal<T>(stream, count));
+        }
+
+        /// <summary>
+        /// Writes the values to the stream.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of the items.
+        /// </typeparam>
+        /// <param name="stream">
+        /// The stream to write to.
+        /// </param>
+        /// <param name="value">
+        /// The items to write.
+        /// </param>
+        /// <returns>
+        /// The number of items written.
+        /// </returns>
+        public async Task<long> WriteAsync<T>(Stream stream, IEnumerable<T> value)
+        {
+            if (!this.writers.TryGetValue(stream, out var writer))
+            {
+                this.writers[stream] = writer = new JsonTextWriter(new StreamWriter(stream));
+            }
+
+            var count = 0L;
+
+            foreach (var obj in value)
+            {
+                this.serializer.Serialize(writer, obj);
+                count++;
+            }
+
+            await writer.FlushAsync();
+
+            return count;
+        }
+
+        /// <summary>
+        /// Reads the items from the stream.
+        /// </summary>
+        /// <param name="stream">
+        /// The stream.
+        /// </param>
+        /// <param name="count">
+        /// The number of items to read.
+        /// </param>
+        /// <typeparam name="T">
+        /// The type of the items.
+        /// </typeparam>
+        /// <returns>
+        /// The items.
+        /// </returns>
+        private IEnumerable<T> ReadAsyncInternal<T>(Stream stream, long count)
+        {
+            if (!this.readers.TryGetValue(stream, out var reader))
+            {
+                this.readers[stream] = reader = new JsonTextReader(new StreamReader(stream))
+                {
+                    SupportMultipleContent = true
+                };
+            }
+
+            while (count-- > 0 && reader.Read() && reader.TokenType == JsonToken.StartObject)
+            {
+                yield return this.serializer.Deserialize<T>(reader);
+            }
+        }
+    }
+}
---
+++ src/ConnectQl.Platform/Plugin.cs
@@ -0,0 +1,100 @@
+// MIT License
+//
+// Copyright (c) 2017 Maarten van Sambeek.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace ConnectQl.Platform
+{
+#if NET45
+    using System.Configuration;
+#endif
+
+    using ConnectQl.FileFormats;
+    using ConnectQl.Interfaces;
+    using ConnectQl.Platform.FileFormats;
+
+    /// <summary>
+    /// The platform plugin.
+    /// </summary>
+    public class Plugin : IConnectQlPlugin
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name => "Platform";
+
+        /// <summary>
+        /// Registers the plugin on the context.
+        /// </summary>
+        /// <param name="context">
+        /// The context to register the plugin on.
+        /// </param>
+        public void RegisterPlugin(IPluginContext context)
+        {
+            context.Functions
+                .AddWithoutSideEffects("connectionstring", (string name) => ConnectionString(name))
+                .SetDescription("Gets a connection string from the application config file.", "The name of the connection string.")
+                .AddWithoutSideEffects("appsetting", (string name) => AppSetting(name))
+                .SetDescription("Gets an application setting from the application config file.", "The name of the application setting.")
+                .AddWithoutSideEffects("separator", (string separator) => separator)
+                .SetDescription("Sets the separator for the CSV file format.", "The separator to use (default: ',').");
+
+            context.FileFormats
+                .Add(new CsvFileFormat())
+                .Add(new JsonFileFormat());
+        }
+
+        /// <summary>
+        /// Gets a connection string from the configuration file.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the connection string.
+        /// </param>
+        /// <returns>
+        /// The connection string or <c>null</c> if it was not found.
+        /// </returns>
+        private static string ConnectionString(string name)
+        {
+#if NETSTANDARD1_5
+            return string.Empty;
+#elif NET45
+            return ConfigurationManager.ConnectionStrings[name]?.ConnectionString;
+#endif
+        }
+
+        /// <summary>
+        /// Gets a application setting from the configuration file.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the connection string.
+        /// </param>
+        /// <returns>
+        /// The connection string or <c>null</c> if it was not found.
+        /// </returns>
+        private static string AppSetting(string name)
+        {
+#if NETSTANDARD1_5
+            return string.Empty;
+#elif NET45
+            return ConfigurationManager.AppSettings[name];
+#endif
+        }
+    }
+}
