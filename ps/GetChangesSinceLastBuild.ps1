c7edf859430e57b7c68a74fb4e0c9ea079ff5e95
Maarten van Sambeek <maarten.x@gmail.com>
2017-07-30 13:31:22 +0200

    Updated powershell script.

--- ps/GetChangesSinceLastBuild.ps1
+++ ps/GetChangesSinceLastBuild.ps1
@@ -6,19 +6,30 @@ param(
     [string] $ApiToken
 )
 
-$api = "https://ci.appveyor.com/api/projects/$($Env:APPVEYOR_ACCOUNT_NAME)/$($Env:APPVEYOR_PROJECT_SLUG)/history?recordsNumber=5000"
-$headers = @{ "Authorization" = "Bearer $apiToken"; "Content-Type" = "application/json" };
-$slash = "$([IO.Path]::DirectorySeparatorChar)";
-
-$json = Invoke-RestMethod -Uri $api -Headers $headers
-$lastBuildCommitId = ($json.builds | ? { $_.status -eq 'success' -and $_.branch -eq $env:APPVEYOR_REPO_BRANCH } | select -First 1).commitId
-$changedFiles = (git diff-tree -r --name-only --no-commit-id $lastBuildCommitId HEAD | Get-Item)
-$changedFolders = $changedFiles.Directory.FullName | % { $_ + $slash }
 $projects = ls *.csproj -r
 
-if ($changedFiles.Name -contains "appveyor.yml" -or $changedFiles.Name -contains "src\ConnectQl.Defaults.targets") {
-    $projects
+if ("$apiToken" -eq "")
+{
+	$result = $projects
 }
-else {
-    $projects | ? { $project = $_; ($changedFolders | ? { $_.StartsWith($project.Directory.FullName + $slash) }).Count -gt 0 }
+else 
+{
+	$api = "https://ci.appveyor.com/api/projects/$($Env:APPVEYOR_ACCOUNT_NAME)/$($Env:APPVEYOR_PROJECT_SLUG)/history?recordsNumber=5000"
+	$headers = @{ "Authorization" = "Bearer $apiToken"; "Content-Type" = "application/json" };
+	$slash = "$([IO.Path]::DirectorySeparatorChar)";
+
+	$json = Invoke-RestMethod -Uri $api -Headers $headers
+	$lastBuildCommitId = ($json.builds | ? { $_.status -eq 'success' -and $_.branch -eq $env:APPVEYOR_REPO_BRANCH } | select -First 1).commitId
+	$changedFiles = (git diff-tree -r --name-only --no-commit-id $lastBuildCommitId HEAD | Get-Item)
+	$changedFolders = $changedFiles.Directory.FullName | % { $_ + $slash }
+
+	if ($changedFiles.Name -contains "appveyor.yml" -or $changedFiles.Name -contains "src\ConnectQl.Defaults.targets") {
+		$result = $projects
+	}
+	else {
+		$result = $projects | ? { $project = $_; ($changedFolders | ? { $_.StartsWith($project.Directory.FullName + $slash) }).Count -gt 0 }
+	}
 }
+
+Write-Host "Packaging projects:"
+Write-Host $result.Name -join "\n"
