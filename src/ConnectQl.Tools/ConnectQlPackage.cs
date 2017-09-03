d9d8bcb2c2402f0194487ea6538e861b230df3bd
Maarten van Sambeek <maarten.x@gmail.com>
2017-09-03 14:32:19 +0200

    Added resources, updated Scripty version to internal build.

--- src/ConnectQl.Tools/ConnectQlPackage.cs
+++ src/ConnectQl.Tools/ConnectQlPackage.cs
@@ -36,6 +36,7 @@ namespace ConnectQl.Tools
     using Microsoft.VisualStudio.Utilities;
     using static Microsoft.VisualStudio.VSConstants;
     using Task = System.Threading.Tasks.Task;
+    using ConnectQl.Tools.Resources;
 
     /// <summary>
     /// The Visual Studio Package.
@@ -105,6 +106,8 @@ namespace ConnectQl.Tools
         /// </returns>
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            var r = Resource.Get("112");
+
             await base.InitializeAsync(cancellationToken, progress);
         }
 
---
+++ src/ConnectQl.Tools/Resources/Resource.cs
@@ -0,0 +1,58 @@
+﻿// MIT License
+//
+// Copyright (c) 2017 Maarten van Sambeek.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace ConnectQl.Tools.Resources
+{
+    using System;
+    using System.Globalization;
+    using System.Resources;
+
+    /// <summary>
+    /// Holds the resources.
+    /// </summary>
+    public static class Resource
+    {
+        /// <summary>
+        /// The resource manager.
+        /// </summary>
+        private static readonly Lazy<ResourceManager> ResourceManager = new Lazy<ResourceManager>(() => new ResourceManager("VSPackage", typeof(Resource).Assembly));
+
+        /// <summary>
+        /// Gets or sets the culture.
+        /// </summary>
+        public static CultureInfo Culture
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets the specified resource.
+        /// </summary>
+        /// <param name="resource">The resource.</param>
+        /// <returns>The resource string.</returns>
+        public static string Get(string resource)
+        {
+            return ResourceManager.Value.GetString(resource, Culture);
+        }
+    }
+}
