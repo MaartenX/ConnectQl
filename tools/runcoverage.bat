7d438485c70c977ae4c6b35f0267d044b3a5cb32
Maarten van Sambeek <maarten.x@gmail.com>
2017-10-22 09:19:24 +0200

    Removed Jetbrains from coverage.

--- tools/runcoverage.bat
+++ tools/runcoverage.bat
@@ -8,8 +8,8 @@ SET err=0
 nuget install OpenCover -ExcludeVersion > NUL
 nuget install Codecov -ExcludeVersion > NUL
 
-OpenCover\tools\OpenCover.Console.exe -oldStyle -register:user "-target:%cwd%\runtests.bat" "-filter:+[ConnectQl*]* -[*Tests*]*" "-output:artifacts\ConnectQl.Coverage.xml" -returntargetcode
-
+OpenCover\tools\OpenCover.Console.exe -oldStyle -register:user "-target:%cwd%\runtests.bat" "-filter:+[ConnectQl*]* -[*Tests*]* -[*]JetBrains.*" "-output:artifacts\ConnectQl.Coverage.xml" -returntargetcode
+s
 IF ERRORLEVEL 1 ( SET err=1 )
 
 Codecov\tools\codecov.exe -f "artifacts\ConnectQl.Coverage.xml"
